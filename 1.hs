3cad2eca9395d629a67fb425bd2aad23efca846f
Alex Ramsdell <alexramsdell@gmail.com>
2017-04-30 12:36:52 -0700

    turn into function

--- 1.hs
+++ 1.hs
@@ -1,4 +1,9 @@
 -- If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 -- Find the sum of all the multiples of 3 or 5 below 1000.
 
-sum [x | x <- [1 .. 999], mod x 3 == 0 || mod x 5 == 0]
+
+sumOfMultiples :: Int -> Int
+sumOfMultiples n = sum [ x |  x <- [1 .. n - 1], x `mod` 3 == 0 || x `mod` 5 == 0 ] 
+
+demo = sumOfMultiples 10
+answer = sumOfMultiples 1000
